fe4ffe4d8d13cf67f92a0628c2d5c49bcd8b6b8f
Dibeja <l.dibella@gmail.com>
2012-02-02 10:25:47 +0100

    Module structure

---
+++ language/english/lang.how_fb_comments.php
@@ -0,0 +1,17 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$lang = array(	
+	'how_fb_comments_module_name' => 
+	'HOW Facebook comments',
+
+	'how_fb_comments_module_description' => 
+	'Manage Facebook comments integration',
+
+	'module_home' => 'HOW Facebook comments Home',
+	
+// Start inserting custom language keys/values here
+	
+);
+
+/* End of file lang.how_fb_comments.php */
+/* Location: /system/expressionengine/third_party/how_fb_comments/language/english/lang.how_fb_comments.php */
---
+++ mod.how_fb_comments.php
@@ -0,0 +1,47 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * ExpressionEngine - by EllisLab
+ *
+ * @package		ExpressionEngine
+ * @author		ExpressionEngine Dev Team
+ * @copyright	Copyright (c) 2003 - 2011, EllisLab, Inc.
+ * @license		http://expressionengine.com/user_guide/license.html
+ * @link		http://expressionengine.com
+ * @since		Version 2.0
+ * @filesource
+ */
+ 
+// ------------------------------------------------------------------------
+
+/**
+ * HOW Facebook comments Module Front End File
+ *
+ * @package		ExpressionEngine
+ * @subpackage	Addons
+ * @category	Module
+ * @author		Handsonweb
+ * @link		http://www.handsonweb.it
+ */
+
+class How_fb_comments {
+	
+	public $return_data;
+	
+	/**
+	 * Constructor
+	 */
+	public function __construct()
+	{
+		$this->EE =& get_instance();
+	}
+	
+	// ----------------------------------------------------------------
+
+	/**
+	 * Start on your custom code here...
+	 */
+	
+}
+/* End of file mod.how_fb_comments.php */
+/* Location: /system/expressionengine/third_party/how_fb_comments/mod.how_fb_comments.php */
